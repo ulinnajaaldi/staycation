018ac0ceb4f7f64523bd50183d724a8f71bcd85a
sgitwhyd <sigitwahyu.mutu@gmail.com>
2022-12-28 13:15:54 +0700

    feat: create place section

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import React, { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-import LandingPage from "./pages/LandingPage";
-import NavigationBar from "./components/navbar/NavigationBar";
-import EmptyPage from "./pages/EmptyPage";
+import LandingPage from './pages/LandingPage';
+import NavigationBar from './components/navbar/NavigationBar';
+import EmptyPage from './pages/EmptyPage';
 
 function App() {
-  const routes = [
-    { path: "/", element: <LandingPage /> },
-    { path: "*", element: <EmptyPage /> },
-  ];
+	const routes = [
+		{ path: '/', element: <LandingPage /> },
+		{ path: '*', element: <EmptyPage /> },
+	];
 
-  return (
-    <div>
-      <NavigationBar />
-      <Routes>
-        {routes.map((route) => (
-          <Route key={route.path} path={route.path} element={route.element} />
-        ))}
-      </Routes>
-    </div>
-  );
+	return (
+		<Routes>
+			{routes.map((route) => (
+				<Route key={route.path} path={route.path} element={route.element} />
+			))}
+		</Routes>
+	);
 }
 
 export default App;
