f27d9b3e85fd24649254e398cc6f6a796a877139
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2022-12-29 10:53:32 +0700

    feat: custom theme on daisyUI

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,6 +12,28 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: false,
+    themes: [
+      {
+        mytheme: {
+          primary: "#3252DF",
+
+          secondary: "#F000B8",
+
+          accent: "#F5F6F8",
+
+          neutral: "#3D4451",
+
+          "base-100": "#FFFFFF",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
   },
 };
