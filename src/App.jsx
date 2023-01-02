b5ef947af58e078ed5eaa36f0fe050dc7f29ce28
sgitwhyd <sigitwahyu.mutu@gmail.com>
2023-01-02 19:46:32 +0700

    feat: create detail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import React, { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-import LandingPage from "./pages/LandingPage";
-import NavigationBar from "./components/navbar/NavigationBar";
-import EmptyPage from "./pages/EmptyPage";
+import LandingPage from './pages/LandingPage';
+import NavigationBar from './components/navbar/NavigationBar';
+import EmptyPage from './pages/EmptyPage';
+import Detail from './pages/detail';
 
 function App() {
-  const routes = [
-    { path: "/", element: <LandingPage /> },
-    { path: "*", element: <EmptyPage /> },
-  ];
+	const routes = [
+		{ path: '/', element: <LandingPage /> },
+		{ path: '*', element: <EmptyPage /> },
+		{
+			path: '/detail/:id',
+			element: <Detail />,
+		},
+	];
 
-  return (
-    <div>
-      <NavigationBar />
-      <Routes>
-        {routes.map((route) => (
-          <Route key={route.path} path={route.path} element={route.element} />
-        ))}
-      </Routes>
-    </div>
-  );
+	return (
+		<div>
+			<NavigationBar />
+			<Routes>
+				{routes.map((route) => (
+					<Route key={route.path} path={route.path} element={route.element} />
+				))}
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
